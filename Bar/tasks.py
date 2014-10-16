63021a0ab330351861f7306dbbba475e79f4e68a
sebastienpro <spro@webgeoservices.com>
2014-10-16 18:18:59 +0200

    seb t'es qu'une burne ....

--- Bar/tasks.py
+++ Bar/tasks.py
@@ -8,11 +8,15 @@ logger = get_task_logger(__name__)
 @task()
 def send_fidelity(products_list):
     for product in products_list:
-        if product['id'] in ['2','4','6','9','10','94','97','98','110','111']:#les pintes
+        print product['id']
+        if product['id'] in [2,4,6,9,10,94,97,98,110,111]:#les pintes
             product['type']="pinte"
-        elif product['id'] in ['1','3','5','7','8','95','96','99']:#les demis
+            print "pinte"
+        elif product['id'] in [1,3,5,7,8,95,96,99]:#les demis
+            print "demi"
             product['type']="demi"
-        elif product['id'] in ['14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34']:
+        elif product['id'] in [14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]:
+            print "bouteille"
             product['type']="bouteille"
         else:
             product['type']="autre"
