d1bf36ee46bcf251bc0fdf64e22358d4296251cb
sebastienpro <spro@webgeoservices.com>
2015-02-28 16:40:58 +0100

    ajout cornet dans les fidelites

--- Bar/tasks.py
+++ Bar/tasks.py
@@ -9,10 +9,10 @@ logger = get_task_logger(__name__)
 def send_fidelity(products_list):
     for product in products_list:
         print product['id']
-        if product['id'] in [2,4,6,9,10,94,97,98,110,111]:#les pintes
+        if product['id'] in [2,4,6,9,10,94,97,98,110,111,112]:#les pintes
             product['type']="pinte"
             print "pinte"
-        elif product['id'] in [1,3,5,7,8,95,96,99]:#les demis
+        elif product['id'] in [1,3,5,7,8,95,96,99,113]:#les demis
             print "demi"
             product['type']="demi"
         elif product['id'] in [14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34]:
