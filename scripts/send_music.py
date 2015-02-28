97de02d343f03e83446ca8cf216e6588f76212d3
sebastienpro <spro@webgeoservices.com>
2015-02-28 17:36:19 +0100

    add try catch

--- scripts/send_music.py
+++ scripts/send_music.py
@@ -6,9 +6,11 @@ import requests
 while 1:
     sleep(15)
     for zone in soco.discover():
-        info = zone.get_current_track_info()
-        if info['metadata'] != 'NOT_IMPLEMENTED':
-            print info
-            URL = 'http://www.dudebar.fr/music'
-            params = dict(login="", password="", data=json.dumps(info))
-            requests.post(URL, data=params, headers=dict(Referer=URL))
+        try:
+            info = zone.get_current_track_info()
+            if info['metadata'] != 'NOT_IMPLEMENTED':
+                URL = 'http://www.dudebar.fr/music'
+                params = dict(login="", password="", data=json.dumps(info))
+                requests.post(URL, data=params, headers=dict(Referer=URL))
+        except:
+            pass
